a566a789da90d2eb05d992ed962f3cf07c6a34fd
ZEKE <101160055+itsZekiee@users.noreply.github.com>
2024-04-30 11:41:57 +0800

    1st Draft

--- assets/js/main_js.js
+++ assets/js/main_js.js
@@ -1,11 +1,45 @@
-const formContainer = document.getElementById('reservation-form');
-const cuisineSelect = document.getElementById('cuisine');
-const subCuisineSelect = document.getElementById('sub-cuisine');
-
-cuisineSelect.addEventListener('change', () => {
-    if (cuisineSelect.value === 'Western') {
+$(document).ready(function(){
+    const formContainer = document.getElementById('reservation-form');
+    const cuisineSelect = document.getElementById('cuisine');
+    const subCuisineSelect = document.getElementById('sub-cuisine');
+  
+    cuisineSelect.addEventListener('change', () => {
+      if (cuisineSelect.value === 'Western') {
         formContainer.classList.add('show-sub-cuisine');
-    } else {
+      } else {
         formContainer.classList.remove('show-sub-cuisine');
-    }
-});
+      }
+    });
+  
+    $(".owl-carousel").owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true,
+      center: true,
+      navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>"
+      ],
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:1
+        },
+        1000:{
+          items:3
+        }
+      },
+      onTranslated: function(event) {
+        var $activeCard = $(event.target).find('.owl-item.active.slider-card');
+        $activeCard.addClass('active');
+        setTimeout(function() {
+          $activeCard.removeClass('active');
+        }, 300); 
+      }
+    });
+  });
